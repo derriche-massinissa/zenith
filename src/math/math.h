0d22895befd728d1ef7428d1c0437873062ddc33
HexOctal <hex0octal@gmail.com>
2021-03-18 17:14:40 +0100

    Revised the Math files

--- src/math/math.h
+++ src/math/math.h
@@ -1,12 +1,12 @@
 /**
- * @file		math.h
+ * @file
  * @author		__AUTHOR_NAME__ <mail@host.com>
  * @copyright	2021 __COMPANY_LTD__
  * @license		<a href="https://opensource.org/licenses/MIT">MIT License</a>
  */
 
-#ifndef MATH_H
-#define MATH_H
+#ifndef ZEN_MATH_H
+#define ZEN_MATH_H
 
 #include <algorithm>
 #include <cmath>
