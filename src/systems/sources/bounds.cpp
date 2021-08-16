29f55012a41ef899c42fd5c5a689cb87f45ce916
HexOctal <hex0octal@gmail.com>
2021-08-16 15:29:58 +0100

    Corrected bound clamp by adding parenthesis

--- src/systems/sources/bounds.cpp
+++ src/systems/sources/bounds.cpp
@@ -51,7 +51,7 @@ double ClampX (Entity entity, double x)
 	auto bounds = g_registry.try_get<Components::Bounds>(entity);
 	ZEN_ASSERT(bounds, "The entity has no 'Bounds' component.");
 
-	double bx = bounds->bounds.x + (GetDisplayWidth(entity) - GetWidth(entity) / 2.);
+	double bx = bounds->bounds.x + ( (GetDisplayWidth(entity) - GetWidth(entity)) / 2.);
 	double bw = std::max(bx, bx + bounds->bounds.width - GetDisplayWidth(entity));
 
 	return Math::Clamp(x, bx, bw);
@@ -62,7 +62,7 @@ double ClampY (Entity entity, double y)
 	auto bounds = g_registry.try_get<Components::Bounds>(entity);
 	ZEN_ASSERT(bounds, "The entity has no 'Bounds' component.");
 
-	double by = bounds->bounds.y + (GetDisplayHeight(entity) - GetHeight(entity) / 2.);
+	double by = bounds->bounds.y + ( (GetDisplayHeight(entity) - GetHeight(entity)) / 2.);
 	double bh = std::max(by, by + bounds->bounds.height - GetDisplayHeight(entity));
 
 	return Math::Clamp(y, by, bh);
